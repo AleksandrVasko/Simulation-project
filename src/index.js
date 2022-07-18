3d888761b9ea29da21a295c26739a582ae63c924
Alexandr Vasko <alexandervasko11@gmail.com>
2022-07-18 17:45:17 +0700

    выполнил задание по 21 уроку

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import inputCheck from './modules/input';
+
 
 timer('20 jule 2022');
 menu();
 modal();
+inputCheck();
