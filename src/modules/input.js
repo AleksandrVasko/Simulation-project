22c3072f41651314d93a1ff238a1efbb49a19e91
Alexandr Vasko <alexandervasko11@gmail.com>
2022-07-18 18:03:02 +0700

    рефакторинг задания

--- src/modules/input.js
+++ src/modules/input.js
@@ -8,33 +8,52 @@ const inputCheck = () => {
     const modalInput = document.querySelectorAll('.form-name')[1];
     const footerInput = document.getElementById('form2-name');
     const messageInput = document.getElementById('form2-message');
+
     const emailInputs = document.querySelectorAll('.form-email');
     const phoneInputs = document.querySelectorAll('.form-phone');
 
     const numberCheck = (input) => {
-        input.addEventListener('input', (e) => {
+        input.addEventListener('blur', (e) => {
             e.target.value = e.target.value.replace(/\D+/, '');
         });
     };
 
     const textCheck = (input) => {
-        input.addEventListener('input', (e) => {
-            e.target.value = e.target.value.replace(/[^а-яё\-\s]/i, '');
+        input.addEventListener('blur', (e) => {
+            let riteWith = e.target.value.replace(/[^а-яё\-\s+]/gi, ' ');
+            riteWith = riteWith.replace(/^\s+|\s+$/g, '');
+            riteWith = riteWith.replace(/^\-+|\-+$/g, '');
+            riteWith = riteWith.replace(/\s+/g, ' ');
+            riteWith = riteWith.replace(/\-+/g, '-');
+
+            e.target.value = riteWith;
+            let riteWithUp = riteWith[0].toUpperCase() + riteWith.slice(1);
+            e.target.value = riteWithUp;
         });
+
+
     };
 
     const mailCheck = (input) => {
-        input.forEach((item) => {
-            item.addEventListener('input', () => {
-                item.value = item.value.replace(/[^a-z\@\-\_\.\!\~\*\']/i, '');
+        input.forEach((e) => {
+            e.addEventListener('blur', () => {
+                let riteWith = e.value.replace(/[^a-z\@\-\_\.\!\~\*\']/i, '');
+                riteWith = riteWith.replace(/^\-+|\-+$/g, '');
+                riteWith = riteWith.replace(/\-+/g, '-');
+
+                e.value = riteWith;
             });
         });
     };
 
     const phoneCheck = (input) => {
-        input.forEach((item) => {
-            item.addEventListener('input', () => {
-                item.value = item.value.replace(/[^0-9\-\(\)]/i, '');
+        input.forEach((e) => {
+            e.addEventListener('blur', () => {
+                let riteWith = e.value.replace(/[^0-9\-\(\)]/gi, '');
+                riteWith = riteWith.replace(/^\-+|\-+$/g, '');
+                riteWith = riteWith.replace(/\-+/g, '-');
+
+                e.value = riteWith;
             });
         });
     };
@@ -47,6 +66,7 @@ const inputCheck = () => {
     textCheck(modalInput);
     textCheck(footerInput);
     textCheck(messageInput);
+
     mailCheck(emailInputs);
     phoneCheck(phoneInputs);
 };
