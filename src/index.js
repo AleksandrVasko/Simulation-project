605cefa58e5d9c1b77ebc282c4e535dd56ab0674
Alexandr Vasko <alexandervasko11@gmail.com>
2022-07-19 15:35:08 +0700

    Выполнил задание по 23 уроку

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import inputCheck from './modules/input';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
-
-timer('20 jule 2022');
+timer('17 jule 2022');
 menu();
 modal();
 inputCheck();
 tabs();
+slider('.portfolio-content', '.portfolio-item', '.portfolio-dots');
