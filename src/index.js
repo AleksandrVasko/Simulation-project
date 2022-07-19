d94425fafc865ffdd04b496fb2cdc451712fdc0f
Alexandr Vasko <alexandervasko11@gmail.com>
2022-07-20 00:09:34 +0700

    Выполнил задание по 24 уроку

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import modal from './modules/modal';
 import inputCheck from './modules/input';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import calc from './modules/calc';
 
-timer('17 jule 2022');
+timer('25 jule 2022');
 menu();
 modal();
 inputCheck();
 tabs();
 slider('.portfolio-content', '.portfolio-item', '.portfolio-dots');
+calc(100);
